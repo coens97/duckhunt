21c0a64f2b547dc9af8659891f78525bedbafb2e
Coen <coen.stange@hotmail.com>
2012-10-07 12:41:06 -0400

    Added difficulty per round but not working yet

---
+++ scripts/rounds.js
@@ -0,0 +1,12 @@
+var rounds = [
+    {maxTime:10000,duckSpeed:5,minDucks:5},//1
+    {maxTime:9000,duckSpeed:6,minDucks:5},//2
+    {maxTime:8000,duckSpeed:6,minDucks:6},//3
+    {maxTime:7000,duckSpeed:7,minDucks:6},//4
+    {maxTime:6000,duckSpeed:7,minDucks:7},//5
+    {maxTime:6000,duckSpeed:8,minDucks:7},//6
+    {maxTime:6000,duckSpeed:8,minDucks:8},//7
+    {maxTime:5000,duckSpeed:9,minDucks:8},//8
+    {maxTime:5000,duckSpeed:9,minDucks:9},//9
+    {maxTime:6000,duckSpeed:10,minDucks:10}//10
+    ];
