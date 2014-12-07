64445645cda39af38b44dd4e0da03e9ad1b857d8
Coen Stange <coen.stange@hotmail.com>
2014-12-07 18:33:19 +0100

    Fixed errors in game

--- scripts/scenes/gameScene.js
+++ scripts/scenes/gameScene.js
@@ -5,18 +5,18 @@ function GameScene(){
         * dont matter what object you put in herea, it needs the function draw()
         * put it in right order
         *************************/
-        bgColor : new rect("42b9fd",0,0,800,600),//the cyan background
+        bgColor : new rect("#42b9fd",0,0,800,600),//the cyan background
         tree : new image("images/tree.png",5,210,173,260),//the tree isn't part of background
         catchingDog : new catchingDog(),
-        ducks : new ducks(),//this will cotain more ducks    
+        ducks : new ducks(),//this will cotain more ducks
         bg : new image("images/back.png",0,395,800,115),//the background is especialy the grass
-        scoreBg : new rect("cb6a00",0,510,800,90),//the score image doesn't have bg color
+        scoreBg : new rect("#cb6a00",0,510,800,90),//the score image doesn't have bg color
         score : new image("images/score.png",150,530,500,50),//the scoreboard
         scoreBoard : new scoreBoard(),//special sprite that will show the score,number of lives and thumber of bullets
-        dog : new dog()//also special sprite    
+        dog : new dog()//also special sprite
         //beware the last object don't need a comma at the end!
         };
-    
+
     this.sceneState = 0;//intro walking dog
     /***scene states******
      * 0:intro
@@ -32,7 +32,7 @@ function GameScene(){
     this.maxShootTime = 10000;
     this.timer;
     this.round = 1;
-    
+
     this.newRound = function(){
         console.log("New round");
         //reset values
@@ -45,18 +45,18 @@ function GameScene(){
         this.waitForDucksHitTheGround = false;
         this.sprites.scoreBoard.create(6);
         this.sprites.ducks.create(this.sprites.ducks.theDucks.length);//create new ducks
-        
+
         //set variables by round
         this.maxShootTime = rounds.getMaxTime(this.round);
         this.sprites.ducks.changeSpeed(rounds.getDuckSpeed(this.round));
         this.sprites.scoreBoard.minDucks = rounds.getMinDucks(this.round);
-        
+
         this.sceneState = 1;
-        
+
     };
     this.setShootTimer = function(){
         clearTimeout(this.timer);
-        this.timer = setTimeout(function(thisObj){thisObj.notAllShot();  
+        this.timer = setTimeout(function(thisObj){thisObj.notAllShot();
             },this.maxShootTime,this);
     };
     this.mouseDown = function(x,y){
@@ -68,18 +68,18 @@ function GameScene(){
                     this.sprites.ducks.theDucks[i].duckDead = true;//change duckState
                     this.deadDucks++;//another duck dead
                     this.roundDeadDucks++;
-                    
+
                     this.sprites.scoreBoard.addScore(500);
-                    
+
                     this.sprites.scoreBoard.scoreDucks[this.currentDuck] = true;//change scoreboard to add a duck
                     this.currentDuck ++;
-                    
+
                     if(this.deadDucks == this.duckCount){
                         this.sprites.catchingDog.x = this.sprites.ducks.theDucks[i].x;//Change position of dog
                         this.sprites.catchingDog.ducks = this.duckCount;
                         this.waitForDucksHitTheGround = true;
                         return;
-                    }                
+                    }
                 }
             }
             if(this.sprites.scoreBoard.gameBullets == 0){
@@ -96,10 +96,10 @@ function GameScene(){
             this.sprites.ducks.letFly();
         }
     };
-    
+
     this.checkDuck = function(duck,x,y){//check if shot on duck
         if(duck.duckDead){
-            return false;    
+            return false;
         }
         var w = 75;
         var h = 74;
@@ -108,15 +108,15 @@ function GameScene(){
         }
         return false;
     };
-    
-    this.newDucks = function(){//when dog have caught the ducks    
+
+    this.newDucks = function(){//when dog have caught the ducks
         this.sprites.scoreBoard.resetBullets();
         this.sprites.ducks.init();
         this.deadDucks = 0;
         this.sceneState = 1;
-        this.sprites.bgColor.color = "42b9fd";
-        this.setShootTimer(); 
-        
+        this.sprites.bgColor.color = "#42b9fd";
+        this.setShootTimer();
+
         this.checkRound();
     };
     this.checkRound = function(){//check if round is over
@@ -134,7 +134,7 @@ function GameScene(){
                var done = true;
                for(var i = 0;i<this.parent.roundDeadDucks;i++){
                     if(!duckScore[i]){
-                        done = false;    
+                        done = false;
                     }
                }
                if(done){
@@ -145,7 +145,7 @@ function GameScene(){
                     }else{
                         this.parent.newRound();
                     }
-                }          
+                }
                 //move the duckScore
                 for(var i = 1; i <10;i++){
                     if(duckScore[i]&&!duckScore[i-1]){//if current duck is true and previous not swap those
@@ -153,7 +153,7 @@ function GameScene(){
                         duckScore[i-1] = true;
                         break;
                     }
-                }               
+                }
             }
         }
     };
@@ -162,16 +162,16 @@ function GameScene(){
         this.sprites.dog.loop();//dog has animation
         this.sprites.catchingDog.loop();//let the catching dog animate to
         if(this.waitForDucksHitTheGround){
-            this.checkForDucksHitTheGround();    
+            this.checkForDucksHitTheGround();
         }
         if(this.sceneState==3){
-            this.roundTransition.loop();    
+            this.roundTransition.loop();
         }
     };
     this.checkForDucksHitTheGround = function(){
         var notOutOfScreen = false;
         for(var i = 0;i < this.sprites.ducks.theDucks.length;i++){
-            if(this.sprites.ducks.theDucks[i].duckDead&&this.sprites.ducks.theDucks[i].y<450){//if a duck is dead and not out of window 
+            if(this.sprites.ducks.theDucks[i].duckDead&&this.sprites.ducks.theDucks[i].y<450){//if a duck is dead and not out of window
                 notOutOfScreen = true;
             }
         }
@@ -180,13 +180,13 @@ function GameScene(){
             this.sceneState = 2;//let the dog catch it
         }
     };
-    
+
     this.draw = function(){
         /***********
          * loop trough all sprites and draw it on canvas
-         ***********/        
+         ***********/
         for(var thisSprite in this.sprites){
-            this.sprites[thisSprite].draw();       
-        } 
+            this.sprites[thisSprite].draw();
+        }
     };
 }
--- scripts/scenes/menuScene.js
+++ scripts/scenes/menuScene.js
@@ -1,14 +1,12 @@
 function MenuScene(){
     this.sprites = {
-        bgColor : new rect("000000",0,0,800,600),//the black background 
+        bgColor : new rect("#000000",0,0,800,600),//the black background
         logo : new image("images/duckhuntLogo.png",144,50,512,200),//draw logo
-        copy : new text("FFFFFF","Coen Stange & Sebastiaan Hoevers & Jeroen Vervoort & Ramon Schreuders",25,560),//laat onze namen zien
-        duck1 : new text("FFFFFF","1 Duck",360,300),
-        duck2 : new text("FFFFFF","2 Ducks",360,350),
-        highscore : new text("0BDB27","Highscore : 0",190,450),
-        made : new text("FFFFFF","Made By", 360,530),
+        duck1 : new text("#FFFFFF","1 Duck",360,300),
+        duck2 : new text("#FFFFFF","2 Ducks",360,350),
+        highscore : new text("#0BDB27","Highscore : 0",190,450),
+        made : new text("#FFFFFF","Made By: Coen Stange", 180,530)
         };
-    this.sprites.copy.font = "14pt duck_hunt";
     this.mouseDown = function(x,y){
         if(this.checkButton(this.sprites.duck1,x,y)){//when 1 is clicked
             this.startGame(1);
@@ -34,18 +32,18 @@ function MenuScene(){
         }
         return false;
     }
-    
+
     this.loop = function(){
-        
+
     };
-    
+
     this.draw = function(){
         /***********
          * loop trough all sprites and draw it on canvas
-         ***********/     
+         ***********/
         this.sprites.highscore.string = "Highscore : "+ highScore.getHighScore();
         for(var thisSprite in this.sprites){
-            this.sprites[thisSprite].draw();       
-        } 
+            this.sprites[thisSprite].draw();
+        }
     };
 }
